bea25434b0fc2b0dccd0d232da4dc33fdbd7b89a
Alphapara97 <ansuman103042@gmail.com>
2022-05-25 12:40:55 -0400

    25rd May 2022

---
+++ Problems/70_Climbing Stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int step1 = 1;
+        int step2 = 1;
+        int temp;
+        for (int i = 0;i<n-1;i++){
+            temp = step1;
+            step1 = step1 +step2;
+            step2 = temp;
+        }
+        return step1;
+    }
+}
+
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Climbing Stairs.
+//        Memory Usage: 38.8 MB, less than 94.21% of Java online submissions for Climbing Stairs.
+
+//https://www.youtube.com/watch?v=Y0lT9Fck7qI
+//fibonacci logic reverse DP
