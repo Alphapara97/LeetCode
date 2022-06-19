a8605583f7a012554dc1fd7d2bf7a62a46fff3b5
Alphapara97 <ansuman103042@gmail.com>
2022-06-19 19:34:47 -0400

    19th June 2022

--- Problems/1695_Maximum Erasure Value.java
+++ Problems/1695_Maximum Erasure Value.java
@@ -1,4 +1,30 @@
 class Solution {
+    public static int maximumUniqueSubarray(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int max = 0, curr= 0;
+        for (int i = 0, j = 0; j < nums.length; j++) {
+            if (set.contains(nums[j])) {
+                while (nums[i] != nums[j]) {
+                    curr -= nums[i];
+                    set.remove(nums[i]);
+                    i++;
+                }
+                if (nums[i] == nums[j]) {
+                    curr -= nums[i];
+                    set.remove(nums[i]);
+                    i++;
+                }
+            }
+            curr += nums[j];
+            set.add(nums[j]);
+            max = Math.max(curr, max);
+        }
+        return max;
+    }
+}
+
+
+class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int maxScore = 0, currScore = 0;
         Set<Integer> set = new HashSet<>();
