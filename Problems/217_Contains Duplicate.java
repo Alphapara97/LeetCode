c1e49cb729b9951e9a8d0f90cd0bc61a94ad7b82
Alphapara97 <ansuman103042@gmail.com>
2022-08-29 15:21:24 -0400

    29th Aug 2022

--- Problems/217_Contains Duplicate.java
+++ Problems/217_Contains Duplicate.java
@@ -10,3 +10,19 @@ class Solution {
         return false;
     }
 }
+
+
+
+class Solution{
+    public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> check = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(check.contains(nums[i])) return true;
+            else
+            {
+                check.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
